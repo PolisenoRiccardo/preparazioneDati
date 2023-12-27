be381b5d7b9baf9d950432a883e51c002c53534c
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2023-12-27 22:33:49 +0000

    punto 6

--- preparazione-dati/src/app/app.module.ts
+++ preparazione-dati/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { MachineComponent } from './machine/machine.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MachineComponent } from './machine/machine.component';
     MachineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
