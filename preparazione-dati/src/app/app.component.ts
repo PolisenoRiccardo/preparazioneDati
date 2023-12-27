0a112626f588f92e1a43930345b5c0b084b66513
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2023-12-27 21:42:52 +0000

    punto 2

--- preparazione-dati/src/app/app.component.ts
+++ preparazione-dati/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'preparazione-dati';
+
+export class UserListComponent implements OnInit {
+  machines: string[]; 
+  constructor() { 
+     this.machines = ['Ruspa', 'Autocarro', 'Gru', 'Sallo', 'Pirls', 'Panda', 'Ruspm', 'Auts', 'Qul', 'Tolo']; 
+  }
+  ngOnInit() {
+  }
 }
