5eb3f7c0f176b5973869353c33d9247e75752487
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2023-12-27 21:47:58 +0000

    punto 2

--- preparazione-dati/src/app/app.component.ts
+++ preparazione-dati/src/app/app.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-export class UserListComponent implements OnInit {
-  machines: string[]; 
-  constructor() { 
-     this.machines = ['Ruspa', 'Autocarro', 'Gru', 'Sallo', 'Pirls', 'Panda', 'Ruspm', 'Auts', 'Qul', 'Tolo']; 
-  }
-  ngOnInit() {
-  }
+export class AppComponent {
+  title = 'preparazione-dati';
 }
--- preparazione-dati/src/app/container/container.component.ts
+++ preparazione-dati/src/app/container/container.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
-export class ContainerComponent {
 
-}
+export class ContainerComponent implements OnInit {
+  machines: string[]; 
+  constructor() { 
+     this.machines = ['Ruspa', 'Autocarro', 'Gru', 'Sallo', 'Pirls', 'Panda', 'Ruspm', 'Auts', 'Qul', 'Tolo']; 
+  }
+  ngOnInit() {
+  }
+  }
+
