c26f921cd890cfe84796c4d19790dc1644f85d92
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2023-12-27 22:00:34 +0000

    punto 5

--- preparazione-dati/src/app/machine/machine.component.ts
+++ preparazione-dati/src/app/machine/machine.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-machine',
   templateUrl: './machine.component.html',
   styleUrls: ['./machine.component.css']
 })
-export class MachineComponent {
 
+export class MachineComponent implements OnInit{
+  @Input() machine: string; 
+  constructor() {
+  }
+  ngOnInit() {
+  }
 }
